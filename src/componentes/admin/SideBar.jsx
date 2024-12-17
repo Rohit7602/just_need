fff96e835c7118ff56bcf693985904bb8d1df8a1
amitjhajhria0100 <amitjhajhria732@gmail.com>
2024-12-17 10:26:03 +0530

    marge

--- src/componentes/admin/SideBar.jsx
+++ src/componentes/admin/SideBar.jsx
@@ -23,13 +23,13 @@ function SideBar() {
       </div>
       <div className="px-[35px]">
         <div className=" mt-5">
-          <h6 className="text-[#00000099] font-normal text-base ps-2.5 ff_sen">Menu</h6>
+          <h6 className="text-[#00000099] font-normal text-base ps-2.5">Menu</h6>
         </div>
         <div className="flex items-center mt-4 group_hover">
           <VerticalVilotSvg />
           <NavLink className="flex items-center py-2.5 px-[15px] gap-5 ms-[5px] w-full">
             <DasboardSvg />
-            <span className="font-normal text-base text-[#00000099] ff_sen">
+            <span className="font-normal text-base text-[#00000099]">
               Dashboard
             </span>
           </NavLink>
@@ -38,7 +38,7 @@ function SideBar() {
           <VerticalVilotSvg />
           <NavLink className="flex items-center py-2.5 px-[15px] gap-5 ms-[5px] w-full">
             <CustomerSvg />
-            <span className="font-normal text-base text-[#00000099] ff_sen">
+            <span className="font-normal text-base text-[#00000099]">
               Customer’s
             </span>
           </NavLink>
@@ -47,7 +47,7 @@ function SideBar() {
           <VerticalVilotSvg />
           <NavLink className="flex items-center py-2.5 px-[15px] gap-5 ms-[5px] w-full">
             <ProviderSvg />
-            <span className="font-normal text-base text-[#00000099] ff_sen">
+            <span className="font-normal text-base text-[#00000099]">
               Providers
             </span>
             <NextArrowSvg />
@@ -57,7 +57,7 @@ function SideBar() {
           <VerticalVilotSvg />
           <NavLink className="flex items-center py-2.5 px-[15px] ms-[5px] w-full">
             <SubsciptionSvg />
-            <span className="font-normal text-base text-[#00000099] ms-5 me-2 ff_sen">
+            <span className="font-normal text-base text-[#00000099] ms-5 me-2">
               Subscription
             </span>
             <NextArrowSvg />
@@ -68,19 +68,19 @@ function SideBar() {
           <VerticalVilotSvg />
           <NavLink className="flex items-center py-2.5 px-[15px] gap-5 ms-[5px] w-full">
             <ServiceRequestSvg />
-            <span className="font-normal text-base text-[#00000099] ff_sen">
+            <span className="font-normal text-base text-[#00000099]">
               Customer’s
             </span>
           </NavLink>
         </div>
         <div className=" my-4">
-          <h6 className="text-[#00000099] font-normal text-base ps-2.5 ff_sen">General</h6>
+          <h6 className="text-[#00000099] font-normal text-base ps-2.5">General</h6>
         </div>
-        <div className="flex items-center group_hover">
+        <div className="flex items-center group_hover duration-500">
           <VerticalVilotSvg />
           <NavLink className="flex items-center py-2.5 px-[15px] gap-5 ms-[5px] w-full">
             <SettingSvg/>
-            <span className="font-normal text-base text-[#00000099] ff_sen">
+            <span className="font-normal text-base text-[#00000099]">
             Setting
             </span>
           </NavLink>
@@ -89,7 +89,7 @@ function SideBar() {
           <VerticalVilotSvg />
           <NavLink className="flex items-center py-2.5 px-[15px] gap-5 ms-[5px] w-full">
             <LogoutSvg/>
-            <span className="font-normal text-base text-[#00000099] ff_sen">
+            <span className="font-normal text-base text-[#00000099]">
             Logout
             </span>
           </NavLink>
