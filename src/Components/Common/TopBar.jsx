a44e8e38233713406fd232ea31ae9058ef704a7f
rinkiboora <rinkiboora691@gmail.com>
2025-01-13 15:32:21 +0530

    updates in change

--- src/Components/Common/TopBar.jsx
+++ src/Components/Common/TopBar.jsx
@@ -1,33 +1,36 @@
-import React, { useState } from "react";
-import {
-  BlackCloseIcon,
-  ChatIcon,
-  NotificationIcon,
-  SearchIcon,
-} from "../../assets/icon/Icon";
-import AdminImage from "../../assets/png/AdminImage.png";
-import { Link, useLocation } from "react-router-dom";
-import { ArrowIcon, CloseIcon } from "../../assets/icon/Icons";
+import React, { useState, useRef, useEffect } from 'react';
+// Import icons and images
+import { BlackCloseIcon, ChatIcon, NotificationIcon, SearchIcon } from '../../assets/icon/Icon';
+import AdminImage from '../../assets/png/AdminImage.png';
+import { Link, useLocation } from 'react-router-dom';
+import { ArrowIcon, CloseIcon } from '../../assets/icon/Icons';
 
 function TopBar() {
   const [isInputVisible, setIsInputVisible] = useState(false);
   const location = useLocation();
+  const searchRef = useRef(null);
 
   const toggleSearchInput = () => {
     setIsInputVisible((prev) => !prev);
   };
 
-  // Regular expression to check for paths ending with a number
-  const showArrowButton =
-    /\/dashboard\/complaints\/complaintsDetails\/\d+$/.test(
-      location.pathname
-    ) ||
-    /\/dashboard\/usersList\/userDetails\/\d+$/.test(location.pathname) ||
-    location.pathname === "/dashboard/setting";
+  const handleClickOutside = (event) => {
+    if (searchRef.current && !searchRef.current.contains(event.target)) {
+      setIsInputVisible(false);
+    }
+  };
 
-  // const goToChatPage = () => {
-  //   navigate('/Chat');
-  // };
+  useEffect(() => {
+    document.addEventListener('mousedown', handleClickOutside);
+    return () => {
+      document.removeEventListener('mousedown', handleClickOutside);
+    };
+  }, []);
+
+  const showArrowButton =
+    /\/dashboard\/complaints\/complaintsDetails\/\d+$/.test(location.pathname) ||
+    /\/dashboard\/usersList\/userDetails\/\d+$/.test(location.pathname) ||
+    location.pathname === '/dashboard/setting';
 
   return (
     <div>
@@ -41,45 +44,40 @@ function TopBar() {
             </div>
           )}
           <div>
-            <p className={"font-medium text-lg xl:text-[22px] capitalize"}>
-              {location.pathname.startsWith("/dashboard/setting/") ? (
+            <p className={'font-medium text-lg xl:text-[22px] capitalize'}>
+              {location.pathname.startsWith('/dashboard/setting/') ? (
                 <p className="text-[#00000099] font-medium text-lg xl:text-[22px] capitalize">
                   setting /
-                  <span className=" text-black ms-1">
-                    {location.pathname
-                      .replace("/dashboard/setting/", "")
-                      .replace("&", " & ")}
+                  <span className="text-black ms-1">
+                    {location.pathname.replace('/dashboard/setting/', '').replace('&', ' & ')}
                   </span>
                 </p>
               ) : (
                 <p>
                   {location.pathname
-                    .replace("/", "")
-                    .replace("dashboard/", "")
+                    .replace('/', '')
+                    .replace('dashboard/', '')
                     .replace(/usersList\/userDetails\/\d+/, "User's Details")
-                    .replace(
-                      /complaints\/complaintsDetails\/\d+/,
-                      "Complaints Details"
-                    )
-                    .replace("&", " & ")
-                    .replace("/", " / ")
-                    .replace("usersList", "Users List")}
+                    .replace(/complaints\/complaintsDetails\/\d+/, 'Complaints Details')
+                    .replace('&', ' & ')
+                    .replace('/', ' / ')
+                    .replace('usersList', 'Users List')}
                 </p>
               )}
             </p>
-            <p className={"font-normal text-xs xl:text-[14px] opacity-[70%]"}>
-              {location.state}
-            </p>
+            <p className={'font-normal text-xs xl:text-[14px] opacity-[70%]'}>{location.state}</p>
           </div>
         </div>
 
         <div className="flex items-center gap-2 xl:gap-4">
           <div
-            className={`flex items-center  h-[40px] rounded-[50px] bg-[#F1F1F1] ${
-              isInputVisible ? "w-[185px] lg:max-w-[330px] xl:w-[330px]" : "max-w-[40px]"
-            } transition-all duration-300`}
-          >
-            <div  onClick={toggleSearchInput} className={`cursor-pointer w-[40px] flex items-center justify-center`}>
+            ref={searchRef}
+            className={`flex items-center h-[40px] rounded-[50px] bg-[#F1F1F1] ${
+              isInputVisible ? 'w-[185px] lg:max-w-[330px] xl:w-[330px]' : 'max-w-[40px]'
+            } transition-all duration-300`}>
+            <div
+              onClick={toggleSearchInput}
+              className="cursor-pointer w-[40px] flex items-center justify-center">
               <SearchIcon />
             </div>
             {isInputVisible && (
@@ -89,23 +87,22 @@ function TopBar() {
                 className="w-full outline-none bg-[#F1F1F1] text-base placeholder:text-base placeholder:font-normal font-normal placeholder:text-[#00000080] rounded-[10px]"
               />
             )}
-            {isInputVisible && (<div onClick={toggleSearchInput} className="px-3"><BlackCloseIcon/></div>)}
+            {isInputVisible && (
+              <div onClick={toggleSearchInput} className="px-3">
+                <BlackCloseIcon />
+              </div>
+            )}
           </div>
           <Link
-            to={"/dashboard/chat"}
-            state={"Plan, prioritize, and accomplish your tasks with ease."}
-          >
+            to={'/dashboard/chat'}
+            state={'Plan, prioritize, and accomplish your tasks with ease.'}>
             <ChatIcon />
           </Link>
           <button>
             <NotificationIcon />
           </button>
           <div className="flex items-center">
-            <img
-              className="w-[40px]"
-              src={AdminImage}
-              alt="image of admin"
-            />
+            <img className="w-[40px]" src={AdminImage} alt="image of admin" />
             <div className="ms-2.5">
               <p className="font-normal text-sm xl:text-base text-[#171717] leading-[20px]">
                 Super Admin
--- src/pages/admin/UserDetails.jsx
+++ src/pages/admin/UserDetails.jsx
@@ -1,34 +1,63 @@
-import React, { useState } from "react";
+import React, { useState } from 'react';
+import { BackArrowIcon, DisalbleIcon } from '../../assets/icon/Icon';
+import GalleryImg1 from '../../assets/png/galleryImg1.png';
+import MechanicImage from '../../assets/png/mechanicImage.png';
+import HouseCleaner from '../../assets/png/houseCleaner.png';
 import {
-  BackArrowIcon,
-  DisalbleIcon,
-
-} from "../../assets/icon/Icon";
-import GalleryImg1 from "../../assets/png/galleryImg1.png";
-import MechanicImage from "../../assets/png/mechanicImage.png";
-import HouseCleaner from "../../assets/png/houseCleaner.png"
-import { EmailIcon, LocationIcon, PhoneIcon, RatingStarIcon, DisableRedicon } from "../../assets/icon/Icons";
-import DisableProviderPopUp from "../../Components/Popups/DisableProviderPopUp";
-import DisablePopUp from "../../Components/Popups/DisablePopUp";
-import EnablePopUp from "../../Components/Popups/EnablePopUp";
+  EmailIcon,
+  LocationIcon,
+  PhoneIcon,
+  RatingStarIcon,
+  DisableRedicon,
+} from '../../assets/icon/Icons';
+import DisableProviderPopUp from '../../Components/Popups/DisableProviderPopUp';
+import DisablePopUp from '../../Components/Popups/DisablePopUp';
+import EnablePopUp from '../../Components/Popups/EnablePopUp';
 
 function UserDetails() {
   const [showPopupDisable, setShowPopupDisable] = useState(false);
-
 
   function handlePopupDisable() {
     setShowPopupDisable(!showPopupDisable);
   }
 
-   const [listings, setListings] = useState([
-    { id: 1, name: "House Cleaner", description: "Lorem ipsum...", rating: 4.2, reviews: 1452, isEnabled: true },
-    { id: 2, name: "Plumber", description: "Lorem ipsum...", rating: 4.5, reviews: 980, isEnabled: true },
-    { id: 3, name: "Electrician", description: "Lorem ipsum...", rating: 4.0, reviews: 870, isEnabled: true },
-    { id: 4, name: "Gardener", description: "Lorem ipsum...", rating: 4.3, reviews: 450, isEnabled: true },
+  const [listings, setListings] = useState([
+    {
+      id: 1,
+      name: 'House Cleaner',
+      description: 'Lorem ipsum...',
+      rating: 4.2,
+      reviews: 1452,
+      isEnabled: true,
+    },
+    {
+      id: 2,
+      name: 'Plumber',
+      description: 'Lorem ipsum...',
+      rating: 4.5,
+      reviews: 980,
+      isEnabled: true,
+    },
+    {
+      id: 3,
+      name: 'Electrician',
+      description: 'Lorem ipsum...',
+      rating: 4.0,
+      reviews: 870,
+      isEnabled: true,
+    },
+    {
+      id: 4,
+      name: 'Gardener',
+      description: 'Lorem ipsum...',
+      rating: 4.3,
+      reviews: 450,
+      isEnabled: true,
+    },
   ]);
 
   const [showPopup, setShowPopup] = useState(false);
-  const [popupType, setPopupType] = useState(""); 
+  const [popupType, setPopupType] = useState('');
   const [currentListingId, setCurrentListingId] = useState(null);
 
   const handlePopup = (id, type) => {
@@ -41,7 +70,7 @@ function UserDetails() {
     setListings((prevListings) =>
       prevListings.map((listing) =>
         listing.id === currentListingId
-          ? { ...listing, isEnabled: popupType === "enable" }
+          ? { ...listing, isEnabled: popupType === 'enable' }
           : listing
       )
     );
@@ -52,98 +81,83 @@ function UserDetails() {
     setShowPopup(false);
   };
 
-
- 
   return (
     <div className="px-4">
       <div className="flex items-center justify-center">
-          <button onClick={handlePopupDisable} className="flex items-center gap-3 bg-[#0832DE] py-2.5 h-[42px] px-3 xl:px-[15px] rounded-[10px]">
-            <DisalbleIcon />
-            <span className="text-white font-normal text-base">
-              Disable Provider
-            </span>
-          </button>
+        <button
+          onClick={handlePopupDisable}
+          className="flex items-center gap-3 bg-[#0832DE] py-2.5 h-[42px] px-3 xl:px-[15px] rounded-[10px]">
+          <DisalbleIcon />
+          <span className="text-white font-normal text-base">Disable Provider</span>
+        </button>
       </div>
-      <div className="flex flex-wrap  mt-[30px] ">
-        <div className="w-10/12 xl:w-5/12 xl:pe-2.5">
-          <div className="bg-[#6C4DEF] px-[30px] py-5 rounded-[10px] ">
+      <div className="xl:flex mt-[30px]">
+        <div className="w-full lg:w-7/12 xl:w-[399px] xl:pe-2.5 lg:flex">
+          <div className="bg-[#6C4DEF] px-[30px] py-5 rounded-[10px] flex-grow flex ">
             <div className="flex items-center">
               <div className="pe-5 border-e-[1px] border-[#FFFFFF66]">
                 <img src={MechanicImage} alt="image of user" />
-                <h1 className="font-medium text-lg text-white mt-2.5 text-center">
+                <h1 className="font-medium lg:text-base xl:text-lg text-white mt-2.5 text-center">
                   Jhon Deo
                 </h1>
-                <h2 className="text-sm font-normal text-white mt-1 text-center">
-                  Mechanic
-                </h2>
+                <h2 className="text-sm font-normal text-white mt-1 text-center">Mechanic</h2>
               </div>
               <div className="ps-5">
                 <div className="flex gap-2.5 items-center">
                   <PhoneIcon />
-                  <h3 className="text-sm font-normal text-white">
-                    + 91 89979 87908
-                  </h3>
+                  <h3 className="text-sm font-normal text-white">+ 91 89979 87908</h3>
                 </div>
                 <div className="flex gap-2.5 items-center mt-2.5">
                   <EmailIcon />
-                  <h3 className="text-sm font-normal text-white">
-                    johndeo12@gmail.com
-                  </h3>
+                  <h3 className="text-sm font-normal text-white">johndeo12@gmail.com</h3>
                 </div>
                 <div className="flex gap-2.5 items-center mt-2.5">
                   <LocationIcon />
-                  <h3 className="text-sm font-normal text-white">
-                    Hisar Haryana B street 352
-                  </h3>
+                  <h3 className="text-sm font-normal text-white">Hisar Haryana B street 352</h3>
                 </div>
               </div>
             </div>
           </div>
         </div>
-        <div className="w-full xl:w-7/12 xl:ps-2.5 mt-3 xl:mt-0">
-          <div className="bg-[#F1F1F1] rounded-[10px] p-[15px] pb-7">
+        <div className="w-full lg:w-7/12 xl:w-[646px] xl:ps-2.5 mt-3 xl:mt-0 flex">
+          <div className="bg-[#F1F1F1] rounded-[10px] p-[15px] pb-7 flex-grow flex flex-col">
             <p className="font-medium text-lg leading-[22px] text-black pb-2.5 border-b-[0.5px] border-dashed border-[#00000066]">
               Business details
             </p>
             <div className="flex items-center mt-3 xl:mt-[15px]">
-              <div className="w-[30%] xl:w-[40%]">
-                <h2 className="font-medium text-sm xl:text-base text-black">
-                  Business Name:
-                </h2>
+              <div className=" w-4/12">
+                <h2 className="font-medium text-sm xl:text-base text-black">Business Name:</h2>
               </div>
-              <div>
+              <div className=" w-10/12">
                 <h2 className="text-[#000000B2] text-sm xl:text-base font-normal">
                   John Car Solutions
                 </h2>
               </div>
             </div>
             <div className="flex items-center mt-3 xl:mt-[15px]">
-              <div className="w-[30%] xl:w-[40%]">
-                <h2 className="font-medium text-sm xl:text-base text-black">
-                  Service Name:
-                </h2>
+              <div className="w-4/12">
+                <h2 className="font-medium text-sm xl:text-base text-black">Service Name:</h2>
               </div>
-              <div>
-                <h2 className="text-[#000000B2] text-sm xl:text-base font-normal">
-                  Mechanic
-                </h2>
+              <div className=" w-10/12">
+                <h2 className="text-[#000000B2] text-sm xl:text-base font-normal">Mechanic</h2>
               </div>
             </div>
-            <div className="flex items-center mt-3 xl:mt-[15px]">
-              <div className="w-[30%] xl:w-[40%]">
-                <h2 className="font-medium text-sm  xl:text-base text-black">
-                  Categories:
-                </h2>
+            <div className="flex items-center mt-3 ">
+              <div className=" w-4/12">
+                <h2 className="font-medium text-sm xl:text-base text-black">Categories:</h2>
               </div>
-              <div>
+              <div className=" w-10/12">
                 <h2 className="text-[#000000B2] text-sm xl:text-base font-normal">
-                  1.Oil Change 2.Parts Repair 3.AC Service + 4 More
+                  1.Oil Change <span className=" ps-[10px]">2.Parts Repair</span>{' '}
+                  <span className=" ps-[10px]">3.AC Service</span>{' '}
+                  <span className=" ps-[10px]">+ 4 More</span>
                 </h2>
               </div>
             </div>
           </div>
         </div>
       </div>
+
       <p className="font-medium text-lg leading-[22px] text-black pb-2.5 border-b-[0.5px] border-dashed border-[#00000066] mt-[30px]">
         Posted Listing
       </p>
@@ -152,24 +166,24 @@ function UserDetails() {
           <div
             key={item.id}
             className="w-6/12 mt-3 xl:mt-[15px] xl:w-3/12 px-3"
-            style={{ opacity: item.isEnabled ? 1 : 0.5 }}
-          >
+            style={{ opacity: item.isEnabled ? 1 : 0.5 }}>
             <div className="border-[0.5px] border-[#0000004D] rounded-[10px]">
               <img className="rounded-[10px] w-full" src={HouseCleaner} alt="Listing" />
               <div className="p-2.5">
                 <div className="flex items-center justify-between">
                   <p className="font-medium text-sm text-black">{item.name}</p>
                   <button
-                    onClick={() =>
-                      handlePopup(item.id, item.isEnabled ? "disable" : "enable")
-                    }
-                  >
-                    {item.isEnabled ? <DisableRedicon /> : <span className="text-xs font-normal text-[#0DA800] opacity-100">Enable</span>}
+                    onClick={() => handlePopup(item.id, item.isEnabled ? 'disable' : 'enable')}>
+                    {item.isEnabled ? (
+                      <DisableRedicon />
+                    ) : (
+                      <span className="text-xs font-normal text-[#0DA800] hover:opacity-100 opacity-100">
+                        Enable
+                      </span>
+                    )}
                   </button>
                 </div>
-                <p className="font-normal text-[10px] text-[#00000099] mt-1">
-                  {item.description}
-                </p>
+                <p className="font-normal text-[10px] text-[#00000099] mt-1">{item.description}</p>
                 <div className="flex items-center gap-1 mt-2">
                   <RatingStarIcon />
                   <h3 className="text-[#000F02] text-[10px] font-normal">
@@ -182,7 +196,7 @@ function UserDetails() {
         ))}
       </div>
 
-      <div className="bg-white rounded-[10px] p-4 mt-3">
+      <div className="bg-white rounded-[10px]  mt-3">
         <p className="font-medium text-lg leading-[22px] text-black pb-2.5 border-b-[0.5px] border-dashed border-[#00000066]">
           Images
         </p>
@@ -191,23 +205,17 @@ function UserDetails() {
             return (
               <div key={index} className="w-[33%] xl:w-[20%] px-3 mt-4">
                 <div>
-                  <img
-                    className="w-full"
-                    src={GalleryImg1}
-                    alt="image of provider"
-                  />
+                  <img className="w-full" src={GalleryImg1} alt="image of provider" />
                 </div>
               </div>
             );
           })}
         </div>
       </div>
-      {showPopupDisable && (
-       <DisableProviderPopUp handlePopupDisable={handlePopupDisable}/>
-      )}
+      {showPopupDisable && <DisableProviderPopUp handlePopupDisable={handlePopupDisable} />}
       {showPopup && (
         <div>
-          {popupType === "disable" ? (
+          {popupType === 'disable' ? (
             <DisablePopUp onConfirm={handleConfirm} onCancel={handleCancel} />
           ) : (
             <EnablePopUp onConfirm={handleConfirm} onCancel={handleCancel} />
