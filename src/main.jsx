82e9afdb8d5d22a54e72130cdcee188953139aad
vikashasm <vikashboora10270@gmail.com>
2025-03-03 14:00:05 +0530

    terms and conditions done

--- src/main.jsx
+++ src/main.jsx
@@ -7,14 +7,17 @@ import { BrowserRouter } from "react-router-dom";
 import AuthProvider from "./store/AuthContext.jsx";
 import SubscriptionProvider from "./store/SubscriptionContext.jsx";
 import ServiceContext from "./store/ServiceContext.jsx";
+import PolicyProvider from "./store/PrivacyPolicy.jsx";
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <ServiceContext>
       <SubscriptionProvider>
         <AuthProvider>
-          <BrowserRouter>
-            <App />
-          </BrowserRouter>
+          <PolicyProvider>
+            <BrowserRouter>
+              <App />
+            </BrowserRouter>
+          </PolicyProvider>
         </AuthProvider>
       </SubscriptionProvider>
     </ServiceContext>
---
+++ src/store/PrivacyPolicy.jsx
@@ -0,0 +1,45 @@
+import React, { createContext, useContext, useState } from "react";
+import { supabase } from "./supabaseCreateClient";
+const PolicyContext = createContext();
+
+export const usePolicyContext = () => useContext(PolicyContext);
+
+
+
+
+
+function PolicyProvider({ children }) {
+   
+    //get policy
+
+    const fetchPolicy = async () => {
+      const { data, error } = await supabase.from("policy").select("*").eq("id",2).single();
+      if(!error){
+        return data
+      }
+
+
+    };
+ 
+  // Add policy
+  const AddPolicy = async (value) => {
+    
+    const { data,error } = await supabase
+      .from("policy")
+      .update({privacy:value.privacy,service:value.service})
+      .eq("id",2);// passing Id hardcode because there is not delete option in admin panel so it will be fix
+    if (error){
+     
+       return true
+    }else {
+        return false
+    }
+ 
+  };
+
+  return <PolicyContext.Provider
+  value={{AddPolicy,fetchPolicy}}
+  >{children}</PolicyContext.Provider>;
+}
+
+export default PolicyProvider;
