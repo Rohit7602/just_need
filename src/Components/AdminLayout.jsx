b0f597373c55ee7bce37a626c835eeb692906973
harish joshi <harishjoshi021120004@gmail.com>
2025-03-22 14:17:45 +0530

    side bar remaining

--- src/Components/AdminLayout.jsx
+++ src/Components/AdminLayout.jsx
@@ -7,7 +7,7 @@ import TopBar from "./Common/TopBar";
 function AdminLayout() {
   return (
     <div className="flex bg-[#f1f1f1] p-4 gap-4 h-screen overflow-hidden">
-      <div className="max-w-[200px] xl:max-w-[270px]  h-screen  ">
+      <div className="max-w-[200px] xl:max-w-[270px]  h-screen pb-4 pl-1">
         <SideBar />
       </div>
       <div className="w-[calc(100vw-320px)] xl:w-calc(100vw-270px)] scrollRemove ">
